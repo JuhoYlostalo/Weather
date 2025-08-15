a69f21ed416b0dc039912c2e85cbf6138fdc9bcd
JuhoYlostalo <juho.ylostalo@gmail.com>
2025-08-15 16:57:17 +0300

    getting data and displaying it

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from tkinter import *
 import tkinter.font as tkFont
 import os
-
+from dotenv import load_dotenv
+import requests
 # weather app
 #gets weather data from api and shows it using tkinter
 
@@ -9,7 +10,29 @@ root=Tk()
 custom_font = tkFont.Font(family="Arial", size=25)
 root.title("Current weather")
 root.config(width=300,height=40)
-weatherLabel=Label(root,text="37C",font=custom_font) 
+
+
+load_dotenv()
+
+api_key = os.getenv("WEATHER_API_KEY")
+lat="60.993"
+lon="24.464"
+
+def getWeatherData(Api_Key,lattitude,longitude):
+    Url = f"https://api.openweathermap.org/data/2.5/weather?lat={lattitude}&lon={longitude}&appid={Api_Key}&units=metric"
+    try:
+        response = requests.get(Url)
+        data=response.json()
+        temp = data["main"]["temp"]
+        return temp
+    except requests.exceptions.RequestException as e:
+        print(f"Error {e}")
+        return None
+    
+data = getWeatherData(api_key,lat,lon)
+
+weatherLabel=Label(root,text=f"{data}°C",font=custom_font) 
 weatherLabel.place(width=300,height=25)
+print(data)
 
 root.mainloop()
