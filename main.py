0734a6d49e646dc3f97752419fcb92d154c2e967
JuhoYlostalo <juho.ylostalo@gmail.com>
2025-08-15 15:49:34 +0300

    tkinter setup

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
+from tkinter import *
+import tkinter.font as tkFont
+import os
+
 # weather app
 #gets weather data from api and shows it using tkinter
+
+root=Tk()
+custom_font = tkFont.Font(family="Arial", size=25)
+root.title("Current weather")
+root.config(width=300,height=40)
+weatherLabel=Label(root,text="37C",font=custom_font) 
+weatherLabel.place(width=300,height=25)
+
+root.mainloop()
